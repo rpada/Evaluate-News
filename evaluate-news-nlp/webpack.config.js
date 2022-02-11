13672e9044eb8b63637864c130355805508a40d9
rpada <70246093+rpada@users.noreply.github.com>
2022-02-10 17:04:54 -1000

    commit

---
+++ evaluate-news-nlp/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const webpack = require("webpack")
+const HTMLwebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/client/index.js',
+    module: {
+    rules: [
+            {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        }
+    ]
+},
+plugins: [
+    new HTMLwebpackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html"
+    }),
+    new CleanWebpackPlugin({
+        // Simulate the removal of files
+        dry: true,
+        // Write Logs to Console
+        verbose: true,
+        // Automatically remove all unused webpack assets on rebuild
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets: false
+})
+]
+}
